eb1526d482afc14ef84f7b6e098bea0e9660274b
Xurshid-Malikov <maliovxurshid@gmail.com>
2022-06-08 11:36:31 +0500

    create index file

---
+++ js/main.js
@@ -0,0 +1,45 @@
+var alish = Number(prompt("Alisher Qancha puling bor? "));
+var oq = document.querySelector(".status");
+var dam = document.querySelector(".user-name");
+var sam = 500 * 11000;
+var meh = 250 * 11000;
+var xar = 120 * 11800;
+var jam = xar + sam + meh;
+
+if(alish >= jam){
+  oq.textContent = " oq yo'l";
+}
+else{
+  dam.textContent = " ozgina sabr qilish kerak bo'lar ekan"
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let AlisherniPuli = Number(prompt("Alisher Qancha puling bor? "));
+// let Xarajatlar =881; 
+
+// if(AlisherniPuli>=Xarajatlar){
+//  alert("Oq yol")
+// }else{
+//  alert("Alisher,ozgina sabr qilish kerak")
+// }
